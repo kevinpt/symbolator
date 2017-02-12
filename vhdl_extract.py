0024d8351bd8942aa5d597a296bdaf5beceb29f5
Kevin Thibedeau <kevin.thibedeau@gmail.com>
2017-02-12 15:24:14 -0500

    Add array type extraction

--- vhdl_extract.py
+++ vhdl_extract.py
@@ -4,11 +4,11 @@ import re
 
 
 class VhdlExtractor(object):
-  def __init__(self, vector_types=set()):
-    self.vector_types = set(('std_ulogic_vector', 'std_logic_vector',
+  def __init__(self, array_types=set()):
+    self.array_types = set(('std_ulogic_vector', 'std_logic_vector',
       'signed', 'unsigned', 'bit_vector'))
-      
-    self.vector_types |= vector_types
+
+    self.array_types |= array_types
 
   def extract_components(self, fname):
     with io.open(fname, encoding='latin-1') as fh:
@@ -20,10 +20,10 @@ class VhdlExtractor(object):
     for l in lines:
       if re.search(r'^\s*component', l, re.IGNORECASE):
         in_comp = True
-        
+
       if in_comp:
         cur_comp.append(l)
-        
+
       if re.search(r'^\s*end(\s+component)*;', l, re.IGNORECASE):
         if in_comp:
           comps.append(cur_comp)
@@ -31,7 +31,7 @@ class VhdlExtractor(object):
         in_comp = False
 
     return [self.parse_component(c) for c in comps]
-    
+
 
   def parse_component(self, comp):
     cname = None
@@ -40,10 +40,10 @@ class VhdlExtractor(object):
     if m:
       cname = m.group(1)
       #print('Name:', cname)
-      
+
     if cname is None:
       return None
-    
+
     in_generic = False
     in_port = False
     glines = []
@@ -52,30 +52,30 @@ class VhdlExtractor(object):
     for l in comp:
       if re.search(r'generic\s*\(', l, re.IGNORECASE):
         in_generic = True
-        
+
       if re.search(r'port\s*\(', l, re.IGNORECASE):
         in_generic = False
         in_port = True
 
       if in_generic:
         glines.append(l)
-        
+
       if in_port:
         plines.append(l)
-        
+
     g_sect = self.parse_generic(glines)
     p_sect = self.parse_port(plines)
-    
+
     cdata = {
       'name': cname,
     }
-    
+
     if len(g_sect) > 0:
       cdata['generic'] = g_sect
     if len(p_sect) > 0:
       cdata['port'] = p_sect
 
-    return cdata      
+    return cdata
 
 
   def parse_generic(self, glines):
@@ -84,11 +84,11 @@ class VhdlExtractor(object):
       m = re.search(r'^\s*(\w+)\s*:\s*(\w+)', l)
       if m:
         pin_type = m.group(2)
-        bus = True if pin_type.lower() in self.vector_types else False
+        bus = True if pin_type.lower() in self.array_types else False
         pin_def = {'name': m.group(1), 'dir':'in', 'type':pin_type, 'bus':bus}
         signals.append(pin_def)
         #signals.append((m.group(1), 'in', m.group(2)))
-        
+
     return signals
 
   def parse_port(self, plines):
@@ -101,27 +101,57 @@ class VhdlExtractor(object):
         if len(signals) > 0: # Save previous section
           sects.append([sect_name, signals])
           signals = []
-        
+
         sect_name = m.group(1).strip()
         if len(sect_name) == 0:
           sect_name = None
-    
-      m = re.search(r'^\s*(\w+)\s*:\s*(\w+)\s+(\w+)', l)
+
+      # Match on "name : dir type"
+      m = re.search(r'^\s*(\w+)\s*:\s*(\w+)\s+(\w+)\s*(\([^)]+\))?', l)
       if m:
         pin_type = m.group(3)
-        bus = True if pin_type.lower() in self.vector_types else False
+        bus = True if pin_type.lower() in self.array_types else False
+
+        pin_range = m.group(4)
+        if pin_range: # Reformat it and append to the type
+          pin_range = pin_range[1:-1]
+          pin_range = pin_range.replace('downto', ':').replace('to', u'\u2799').replace(' ', '')
+          pin_type = '{}[{}]'.format(pin_type, pin_range)
+
+        #print('## RANGE:', pin_range)
         pin_def = {'name': m.group(1), 'dir':m.group(2), 'type':pin_type, 'bus':bus}
         signals.append(pin_def)
         #signals.append((m.group(1), m.group(2), m.group(3)))
-        
+
     if len(signals) > 0: # Save previous section
       sects.append([sect_name, signals])
-      
+
     return sects
 
-      
+
     #print(lines)
-    
+
+  def extract_array_types(self, files):
+    subtypes = {}
+    for fname in files:
+      with io.open(fname, encoding='latin-1') as fh:
+        for l in fh:
+          m = re.match(r'\s*type\s+(\w+)\s+is\s+array',l, re.IGNORECASE)
+          if m:
+            self.array_types.add(m.group(1))
+          m = re.match(r'\s*subtype\s+(\w+)\s+is\s+(\w+)', l, re.IGNORECASE)
+          if m:
+            subtypes[m.group(1)] = m.group(2)
+
+    # Find all subtypes of an array type
+    for k,v in subtypes.iteritems():
+      while v in subtypes: # Follow subtypes of subtypes
+        v = subtypes[v]
+      if v in self.array_types:
+        self.array_types.add(k)
+
+    #print('## VECTORS:', self.array_types)
+
 
 if __name__ == '__main__':
   comps = extract_components('reg_file.vhdl')
